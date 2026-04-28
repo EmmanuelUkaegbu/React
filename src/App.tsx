501f727d7d65dc69d3feafba9baa91b714bb0b71
EmmanuelUkaegbu <eukaegbu005@gmail.com>
2026-04-28 11:16:56 +0100

    Emmanuel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import Courses from "./Components/Courses/Courses";
-
+import Whobo from "./Components/Whobo/Whobo";
 function App() {
   return (
     <>
+      <Whobo />
       <Courses />
     </>
   );
---
+++ src/Components/Whobo/Whobo.tsx
@@ -0,0 +1,9 @@
+import "./whobo.css";
+const Courses = () => {
+  return (
+    <div>
+      <h1 className="whoba">Welcome to Whoba ogo Foundation</h1>
+    </div>
+  );
+};
+export default Courses;
